1f6489f079f2f0457ff4fcd4efa0f55c63c8d6ef
jonas <jqh@kvalitetsit.dk>
2023-04-27 10:20:22 +0200

    fixed a tiny issue with the token generator

--- integrationtest/src/test/java/dk/kvalitetsit/stakit/integrationtest/TokenGenerator.java
+++ integrationtest/src/test/java/dk/kvalitetsit/stakit/integrationtest/TokenGenerator.java
@@ -17,7 +17,20 @@ import java.util.Date;
 
 public class TokenGenerator {
     static String generateSignedToken() throws NoSuchAlgorithmException, InvalidKeySpecException, URISyntaxException, IOException {
-        return TokenGenerator.generateSignedToken();
+        var privateKeyBytes = Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("key.pkcs8").toURI()));
+
+        var spec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        var privateKey = kf.generatePrivate(spec);
+
+        return Jwts.builder()
+                .setNotBefore(new Date())
+                .setExpiration(Date.from(LocalDateTime.now().plusHours(1L).toInstant(ZoneOffset.UTC)))
+                .setAudience("audience")
+                .setIssuer("issuer")
+                .claim("email", "john@example.com")
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
     }
 
 }
