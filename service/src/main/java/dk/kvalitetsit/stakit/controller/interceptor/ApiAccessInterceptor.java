e09833499b922f0af989dece831c6d3d3921b07d
fma-spec <fma@kvalitetsit.dk>
2026-08-05 09:04:42 +0200

    Migrate springframework.boot from 3.5.16 to 4.1.0

--- service/src/main/java/dk/kvalitetsit/stakit/controller/interceptor/ApiAccessInterceptor.java
+++ service/src/main/java/dk/kvalitetsit/stakit/controller/interceptor/ApiAccessInterceptor.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.webmvc.autoconfigure.error.BasicErrorController;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
